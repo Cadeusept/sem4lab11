aee52dc3fc0edc1fc98f5e5f110476ad8988673a
cadeusept <matwey.likhota@gmail.com>
2022-05-13 23:00:37 +0300

    work finished

--- include/cmake_builder.hpp
+++ include/cmake_builder.hpp
@@ -4,16 +4,16 @@
 #define INCLUDE_CMAKE_BUILDER_HPP_
 
 #include <stdexcept>
-#include <async++.h>
+#include <../third-party/asyncplusplus/include/async++.h>
 #include <boost/process.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <string>
 
 namespace bp = boost::process;
 
-void create_child(const std::string command, size_t timeout);
+void create_child(std::string command, size_t timeout);
 
-void create_child(const std::string command, const time_t &timeout, int &res);
+void create_child(std::string command, const time_t &timeout, int &res);
 
 void check_time(bp::child &process, const time_t &timeout);
 
